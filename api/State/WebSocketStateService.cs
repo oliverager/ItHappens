1fc1eb9c88135dbd2ad557ee941bae8f2f0aea75
Oliver Ager Jørgensen <114471849+oliverager@users.noreply.github.com>
2024-04-23 11:35:48 +0200

    ClientWantsToSignup.cs

---
+++ api/State/WebSocketStateService.cs
@@ -0,0 +1,20 @@
+﻿using Fleck;
+
+namespace api.State;
+
+public class WsWithMetadata(IWebSocketConnection connection)
+{
+    public IWebSocketConnection Connection { get; set; } = connection;
+    public string Username { get; set; }
+}
+
+public static class WebSocketStateService
+{
+    public static Dictionary<Guid, IWebSocketConnection> Connections = new();
+
+    public static bool AddConnection(IWebSocketConnection ws)
+    {
+        //return Connections.TryAdd(ws.ConnectionInfo.Id, new WsWithMetadata(ws));
+        throw new Exception();
+    }
+}
