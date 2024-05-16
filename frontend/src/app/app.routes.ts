0fadcaf0d5a8db8e9964dd70226abcd671f26507
Warfor <44639824+Warfor@users.noreply.github.com>
2024-05-16 21:15:38 +0200

    første udkast til de 2 nye pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import {HomeComponent} from "./home/home.component";
 import {AssociationsComponent} from "./associations/associations.component";
 import {AssociationsPageComponent} from "./associations-page/associations-page.component";
 import {EventPageComponent} from "./event-page/event-page.component";
+import {ProfilePageComponent} from "./profile-page/profile-page.component";
+import {JoinEventComponent} from "./join-event/join-event.component";
 
 
 
@@ -13,7 +15,9 @@ import {EventPageComponent} from "./event-page/event-page.component";
 export const routes: Routes = [
   {path: 'user-login', component: UserLoginComponent},
   {path: 'user-signup', component: UserSignupComponent},
+  {path: 'profile-page', component: ProfilePageComponent},
   {path: 'associations', component: AssociationsComponent},
+  {path: 'join-event/id', component: JoinEventComponent},
   {path: 'app-associations-page/:id', component: AssociationsPageComponent},
   {path: 'app-event-page/:id', component: EventPageComponent},
   {path: 'home', component: HomeComponent},
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
-import {State} from "../../state";
+import {Component, inject,} from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {State} from "../../state";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+import {WebSocketClientService} from "../ws.client.service";
+
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-constructor(public state: State) {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  wsService = inject(WebSocketClientService)
+  constructor(public state: State, private router: Router) {
+    const joinEventId = Number(this.route.snapshot.params['id']);
 
-}
+  }
+  goToEvent(): void {
+    // Assuming you have the ID available in your component
+    const id = 'id'; // Replace 'your-id-value' with your actual ID
 
-
+    // Navigate to the specified route with the ID parameter
+    this.router.navigate(['join-event/id'])
+  }
 }
