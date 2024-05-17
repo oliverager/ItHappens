3e08db2a1bffb3a286b9e739a656510423681fa5
amorags <115067025+amorags@users.noreply.github.com>
2024-05-17 08:59:20 +0200

    EmailService maybe working

---
+++ service/Services/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace service.Services;
+
+    
+
+public static class EmailService
+{
+    public static async Task SendEmailAsync(string toEmail, string subject, string body)
+    {
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential("random@gmail.com", "app-password"),
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("random@gmail.com"),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(toEmail);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
