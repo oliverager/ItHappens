04427ca92bb5c54b3e66df4612484aa14f308805
amorags <115067025+amorags@users.noreply.github.com>
2024-05-23 13:16:59 +0200

    CreateOwnerWorks

---
+++ api/ClientWants/ClientWantsToSignupOwner.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Json;
+using api.State;
+using Fleck;
+using lib;
+using service.Services;
+
+namespace api.ClientWants;
+
+public class ClientWantsToSignupOwnerDto : BaseDto
+{
+    public string username { get; set; }
+    public string firstname { get; set; }
+    public string lastname { get; set; }
+    public string password { get; set; }
+    public string email { get; set; }
+    public int phone { get; set; }
+    
+    public int userType_id { get; set;}
+    
+    public int association_id {get; set;}
+}
+
+public class ClientWantsToSignupOwner : BaseEventHandler<ClientWantsToSignupOwnerDto>
+{
+    private readonly AccountService _accountService;
+    public ClientWantsToSignupOwner(AccountService accountService)
+    {
+        _accountService = accountService;
+        
+    }
+    public override Task Handle(ClientWantsToSignupOwnerDto dto, IWebSocketConnection socket)
+    {
+        
+        if (!WebSocketStateService.Connections.ContainsKey(socket.ConnectionInfo.Id))
+        {
+            WebSocketStateService.AddConnection(socket);
+        }
+        
+        // Set the username for the socket connection
+        WebSocketStateService.Connections[socket.ConnectionInfo.Id].userName = dto.email;
+
+        try
+        {
+            _accountService.RegisterOwner(dto.username, dto.firstname, dto.lastname, dto.email, dto.phone, dto.userType_id, dto.password, dto.association_id);
+            var welcomesmessage = new ServerWelcomesNewUser()
+            {
+                message = $"Welcome to the platform, " + dto.username, eventType = "success"
+            };
+            var messageToClient = JsonSerializer.Serialize(welcomesmessage);
+            socket.Send(messageToClient);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            var errormessage = new ServerWelcomesNewUserOwner()
+            {
+                message = $"Failed to add " + dto.username, eventType = "error"
+            };
+            var messageToClient = JsonSerializer.Serialize(errormessage);
+            socket.Send(messageToClient);
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
+public class ServerWelcomesNewUserOwner : BaseDto
+{
+    public string message { get; set; }
+    
+}
+
--- frontend/src/app/user-signup/user-signup.component.ts
+++ frontend/src/app/user-signup/user-signup.component.ts
@@ -8,8 +8,10 @@ import {
   Validators
 } from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import { TokenServiceService } from '../../../serviceAngular/token-service.service';
 import {WebSocketClientService} from "../ws.client.service";
 import {ClientWantsToSignup} from "../../models/ClientWantsToSignup";
+import {ClientWantsToSignupOwner} from "../../models/ClientWantsToSignupOwner";
 const passwordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const value: string = control.value;
   const hasUpperCase = /[A-Z]/.test(value);
@@ -34,16 +36,21 @@ export class UserSignupComponent {
   ws = inject(WebSocketClientService);
   userForm: FormGroup;
   isFormSubmitted: Boolean = false;
-  constructor(public router: Router) {
+
+  constructor(public router: Router, private tokenService: TokenServiceService) {
     this.userForm = new FormGroup({
       userName: new FormControl("", [Validators.required]),
       firstName: new FormControl("", [Validators.required]),
       lastName: new FormControl("", [Validators.required,Validators.minLength(4)]),
       email: new FormControl("", [Validators.required, Validators.email]),
       phoneNumber: new FormControl("", [Validators.required, Validators.pattern("[0-9]{8}")]),
-      password: new FormControl("", [Validators.required, passwordValidator])
-    })
+      password: new FormControl("", [Validators.required, passwordValidator]),
+      association_id: new FormControl ("", )
+    });
+
+    // Always initialize userOwnerForm with the association_id FormControl
   }
+
   onSubmit(){
     if (this.userForm.valid) {
       this.signUp();
@@ -54,9 +61,30 @@ export class UserSignupComponent {
     }
   }
 
+  onSubmitOwner(){
+    if (this.userForm.valid) {
+      this.signUpOwner();
+      console.log("Success")
+      this.router.navigate(["/user-login"]);
+    } else {
+      console.log("Failed to add user")
+    }
+  }
+
+  getUserRole(): string | null {
+    return this.tokenService.getUserRole();
+  }
+
   signUp(){
     this.ws.socketConnection.sendDto(new ClientWantsToSignup(this.userForm.value!,))
   }
+
+  signUpOwner() {
+
+    this.ws.socketConnection.sendDto(new ClientWantsToSignupOwner(this.userForm.value!,));
+
+  }
+
 }
 
 
